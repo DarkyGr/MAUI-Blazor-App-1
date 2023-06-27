43aca91ff1ea192de748b4d02f6b7711c06f5132
Guillermo Riaño <guilledark17@gmail.com>
2023-06-27 12:03:44 -0600

    ✅ RickAndMorty Interface

---
+++ Maui_BlazorApp_1/Services/IRickAndMorty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Maui_BlazorApp_1.Models;
+
+
+namespace Maui_BlazorApp_1.Services
+{
+    internal interface IRickAndMorty
+    {
+        public Task<RickAndMorty> Get();
+    }
+}
